eabfd971b4d98ff002a5a7e6adc08b027d3b76c7
toastonrye <toastonrye@hotmail.com>
2022-11-04 18:05:53 -0500

    Update meBridge.lua

--- meBridge.lua
+++ meBridge.lua
@@ -1,10 +1,26 @@
-local filePath = "basalt.lua"
+local filePath = "basalt"
 if not(fs.exists(filePath))then
     shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua master")
 end
 -- ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 local basalt = require(filePath)
 
+local function fancyButton(button)
+    button:onClick(function(self)
+        button:setBackground(colours.black)
+        button:setForeground(colours.lightGrey)
+    end)
+    button:onClickUp(function(self)
+        button:setBackground(colours.grey)
+        button:setForeground(colours.black)
+    end)
+    button:onLoseFocus(function(self)
+        button:setBackground(olours.grey)
+        button:setForeground(colours.black)
+    end)
+end
+
+fancyButton(mainF:addButton("testButton"):setPosition(3,2):setValue("test button")
 
 local me = peripheral.find("meBridge")
 if not me then error("meBridge not found") end
