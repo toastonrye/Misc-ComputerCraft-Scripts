01e1ef700cea4a8c2999b60ef9e03e05920243fb
toastonrye <toastonrye@hotmail.com>
2022-11-04 17:57:02 -0500

    Update meBridge.lua

--- meBridge.lua
+++ meBridge.lua
@@ -2,6 +2,7 @@ local filePath = "basalt.lua"
 if not(fs.exists(filePath))then
     shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua master")
 end
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local me = peripheral.find("meBridge")
 if not me then error("meBridge not found") end
@@ -11,3 +12,11 @@ patterns = me.listCraftableItems()
 for k, items in pairs(patterns) do
  print(items.name, items.amount)
 end
+
+local mainF = basalt.createFrame("mainF")
+    :show()
+    :setBackground(colours.purple)
+
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+basalt.debug("Hi")
+basalt.autoUpdate()
