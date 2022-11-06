43195c28220b0b0d811ab6a2fb2df4525492dae3
toastonrye <toastonrye@hotmail.com>
2022-11-06 09:23:39 -0600

    Update meBridge.lua
    Broken basalt lua 571 mad about getSize???

--- meBridge.lua
+++ meBridge.lua
@@ -9,12 +9,13 @@ local basalt = require("basalt")
 local me = peripheral.find("meBridge")
 if not me then error("meBridge not found") end
 
-local ae2RawData = {} -- meBridge peripheral, isItemCraftable table variable. Bug: Only returns if item amount >=1 ?
-local ae2ParsedData = {}
+-- meBridge peripheral, isItemCraftable table variable. Bug: Only returns if item amount >=1 ?
+local parsedData = {}
 
 local w, h = term.getSize()
 local mainF = basalt.createFrame():show():setBackground(colours.purple)
-local pollF = mainF.addFrame():setSize(w-2,h):setPosition(2,5):setBackground(colours.yellow)
+local pollF = mainF.addFrame("test"):setSize(w-2,h):setPosition(2,5):setBackground(colours.yellow)
+--local itemF = pollF.addFrame():setSize(w-4,1):setPosition(2,2):setBackground(colours.red)
 local pollingProgress = mainF:addProgressbar():setPosition(1,4):setBackground(colours.white):setProgress(10)
 -- -------------------------------------------------------------------------------------------------------------------
 local function fancyButton(self, event, button, x, y)
@@ -32,34 +33,96 @@ local function fancyButton(self, event, button, x, y)
     end)
 end
 -- -------------------------------------------------------------------------------------------------------------------
-local function scanCraftableItems(setpoints)
-	local parsed = {}
+local function loadInterface(parsed)
+	local items = {}
+	
+	for i = 1, #parsed do
+		--basalt.debug(i)
+		--basalt.debug("loadInterface " .. k, v.name, v.setpoint)
+		--items = frame:addFrame():setBackground(colours.cyan):setPosition(0+(i*2), 2):setSize(,):setValue("test")
+	end
+	
+	
+
+end
+-- -------------------------------------------------------------------------------------------------------------------
+local function loadConfig()
+	local cfg
+	local f = fs.open("meBridge.txt", "r")
+	if f ~= nil then -- if meBridge.txt exists, load it
+		local data = f.readAll()
+		f.close()
+		cfg = textutils.unserialize(data)
+	end
+	return cfg
+end
+
+-- scans ae2 and loads user setpoints if they exist or sets to 1
+-- need to explain why the redstone pulse
+local function scanCraftableItems()
+	local parsed = {} -- new table to hold name, amount, setpoint
+	local config = loadConfig() -- load any existing user setpoints, or set to 1
+	local ae2Raw, loadedSetpoint
 	
 	redstone.setOutput("back", true)
 	os.sleep(1)
 	
-	local raw = me.listCraftableItems()
-	local spFound
+	ae2Raw = me.listCraftableItems()
 	
-	for k, v in pairs(raw) do
-		if setpoints ~= nil then
-			for k2, v2 in pairs(setpoints) do
+	for k, v in pairs(ae2Raw) do
+		if config ~= nil then
+			for k2, v2 in pairs(config) do
 				if v.name == v2.name then
-					spFound = v2.setpoint
-					basalt.debug("here")
+					loadedSetpoint = v2.setpoint
 				end
 			end
 		end
-		table.insert(parsed, {name = v.name, amount = v.amount, setpoint = spFound or 1})
+		table.insert(parsed, {name = v.name, amount = v.amount, setpoint = loadedSetpoint or 1})
 	end
 	
 	redstone.setOutput("back", false)
-	
 	basalt.debug("Redstone Off")
-	basalt.debug(parsed)
+	
+	local f = fs.open("meBridge.txt", "w")
+	f.write(textutils.serialize(parsed))
+	f.close()
+	basalt.debug("Saved!")
 	
 	return parsed
 end
+
+local function init(frame)
+	local p = scanCraftableItems()	
+	local interface = loadInterface(p) -- pass the data and frame to draw it on
+	
+	return p
+end
+
+local buttonRescan = mainF:addButton():onClick(basalt.schedule(function() parsedData = scanCraftableItems() end)):setSize(10,3):setPosition(1,1):setValue("Rescan")
+fancyButton(buttonRescan)
+
+local function myMain()
+	while true do
+		os.sleep(1)
+	end
+end
+
+parsedData = init() -- try to read meBridge.txt if it exists, if not call scanCraftableItems() function
+-- -------------------------------------------------------------------------------------------------------------------
+--basalt.debug("Hi")
+parallel.waitForAll(basalt.autoUpdate, myMain)
+
+--[[
+
+for k,v in pairs(p) do
+	basalt.debug("init " .. k, v.name, v.setpoint)
+end
+
+local buttonAppend = mainF:addButton():onClick(function() appendMe(ae2RawData) end):setSize(10,3):setPosition(36,1):setValue("Append")
+fancyButton(buttonAppend)
+
+local buttonSave = mainF:addButton():onClick(function() saveMe(ae2RawData) end):setSize(10,3):setPosition(12,1):setValue("Save")
+fancyButton(buttonSave)
 
 local function saveMe(p)
 	for k, v in pairs(p) do
@@ -73,7 +136,6 @@ local function saveMe(p)
 	basalt.debug("Saved!")
 end
 
--- appendME() should be the main saving function??                        -- NEW PLAN program will read whats visible and add setpoints.
 local function appendMe(p)
 	local f = fs.open("meBridge.txt", "r")
 	local d = f.readAll()
@@ -105,64 +167,4 @@ local function appendMe(p)
 	f.close()
 	basalt.debug("Appended!")
 end
-
-local function loadSetpoints()
-	local f = fs.open("meBridge.txt", "r")
-	if f ~= nil then -- if meBridge.txt exists, load it
-		local data = f.readAll()
-		f.close()
-		local p = textutils.unserialize(data)
-		return p
-	end
-end
-
-local function init()
-	local setpoints = loadSetpoints()
-	local parsed = scanCraftableItems(setpoints)
-	
-	local f = fs.open("meBridge.txt", "w")
-	f.write(textutils.serialize(parsed))
-	f.close()
-	basalt.debug("Saved!")
-	
-	--[[local f = fs.open("meBridge.txt", "r")
-	if f ~= nil then -- if meBridge.txt exists, load it
-		local d = f.readAll()
-		f.close()
-		local p = textutils.unserialize(d)
-	else -- if meBridge.txt does not exist, make empty file
-		local f = fs.open("meBridge.txt", "w")
-		f.write()
-		f.close()
-	end
-	
-	if p == nil then
-		basalt.debug("p read from polling")
-		p = scanCraftableItems()
-	else
-		basalt.debug("p loaded from file")
-	end
-	return p
-	--]]
-end
-
-
-local buttonRescan = mainF:addButton():onClick(basalt.schedule(function() ae2RawData = scanCraftableItems() end)):setSize(10,3):setPosition(1,1):setValue("Rescan")
-fancyButton(buttonRescan)
-
-local buttonAppend = mainF:addButton():onClick(function() appendMe(ae2RawData) end):setSize(10,3):setPosition(36,1):setValue("Append")
-fancyButton(buttonAppend)
-
---local buttonSave = mainF:addButton():onClick(function() saveMe(ae2RawData) end):setSize(10,3):setPosition(12,1):setValue("Save")
---fancyButton(buttonSave)
-
-local function myMain()
-	while true do
-		os.sleep(1)
-	end
-end
-
-ae2RawData = init() -- try to read meBridge.txt if it exists, if not call scanCraftableItems() function
--- -------------------------------------------------------------------------------------------------------------------
---basalt.debug("Hi")
-parallel.waitForAll(basalt.autoUpdate, myMain)
+--]]
