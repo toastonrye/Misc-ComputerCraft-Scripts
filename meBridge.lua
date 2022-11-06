8cc3dd90f12bc58754efd954949dd86b500de4f2
toastonrye <toastonrye@hotmail.com>
2022-11-06 07:44:37 -0600

    Update meBridge.lua
    Last version of semi working code for loading/appending.
    Going to change logic so program won't load all data about meBridge, only custom setpoints/name

--- meBridge.lua
+++ meBridge.lua
@@ -1,20 +1,21 @@
+-- -------------------------------------------------------------------------------------------------------------------
 local filePath = "basalt.lua"
 if not(fs.exists(filePath))then
     shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua master")
 end
--- ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------------------------------
 local basalt = require("basalt")
 
 local me = peripheral.find("meBridge")
 if not me then error("meBridge not found") end
 
-local pollCraftable = false
-local tPatterns = {}
+local tPatterns = {} -- meBridge peripheral, isItemCraftable table variable. Bug: Only returns if item amount >=1 ?
 
 local w, h = term.getSize()
 local mainF = basalt.createFrame():show():setBackground(colours.purple)
 local pollF = mainF.addFrame():setSize(w-2,h):setPosition(2,5):setBackground(colours.yellow)
-
+local pollingProgress = mainF:addProgressbar():setPosition(1,4):setBackground(colours.white):setProgress(10)
+-- -------------------------------------------------------------------------------------------------------------------
 local function fancyButton(self, event, button, x, y)
     self:onClick(function()
         self:setBackground(colours.black)
@@ -29,7 +30,7 @@ local function fancyButton(self, event, button, x, y)
         self:setForeground(colours.black)
     end)
 end
-
+-- -------------------------------------------------------------------------------------------------------------------
 local function pollPatterns()
 	redstone.setOutput("back", true)
 	local raw
@@ -41,6 +42,7 @@ local function pollPatterns()
 	end
 	redstone.setOutput("back", false)
 	basalt.debug("Redstone Off")
+	basalt.debug(parsed)
 	return parsed
 end
 
@@ -56,64 +58,77 @@ local function saveMe(p)
 	basalt.debug("Saved!")
 end
 
+-- appendME() should be the main saving function??                        -- NEW PLAN program will read whats visible and add setpoints.
 local function appendMe(p)
 	local f = fs.open("meBridge.txt", "r")
-	local load = textutils.unserialize(f.readAll())
+	local d = f.readAll()
 	f.close()
-	basalt.debug("COMPARISON")
+	local load = textutils.unserialize(d)
+	
 	local m = false
-	if not load then
+	if load == nil then -- if meBridge empty/non-existent, set it to recent polling data
 		load = p
-	else
+	else -- if file not empty, try to append missing keys
 		for k, v in pairs(p) do
-			if v.name == load[k].name and not m then
-				--basalt.debug(v.name .. " = " .. load[k].name)
-				m = true
+			for k2, v2 in pairs(load) do
+				if v.name == v2.name and not m then
+					--basalt.debug(v.name .. " = " .. load[k].name)
+					m = true
+				end
+				if not m then
+					table.insert(load, v) -- THE PROBLEM??
+					basalt.debug("Adding " .. v.name .. " | " .. v.amount .. " | " .. v.setpoint)
+				end
+				m = false
 			end
-			if not m then
-				table.insert(load, v)
-				basalt.debug("Adding " .. v.name .. " | " .. v.amount .. " | " .. v.setpoint)
-			end
-			m = false
 		end
 	end
-	local f = fs.open("meBridge.txt", "w")
+	
+	local f = fs.open("meBridge.txt", "w") -- do not append, values inserted into "load"
 	f.write(textutils.serialize(load))
+	--f.write(load)
 	f.close()
 	basalt.debug("Appended!")
 end
 
-local function loadMe()
+local function init()
 	local f = fs.open("meBridge.txt", "r")
-	local d = f.readAll()
-	f.close()
-	local p = textutils.unserialize(d)
-	--table.remove(p, 2)
+	if f ~= nil then -- if meBridge.txt exists, load it
+		local d = f.readAll()
+		f.close()
+		local p = textutils.unserialize(d)
+	else -- if meBridge.txt does not exist, make empty file
+		local f = fs.open("meBridge.txt", "w")
+		f.write()
+		f.close()
+	end
 	
-	if not p then
-		return -- Does this return have the potential to break parallel.waiteForAll ???
+	if p == nil then
+		basalt.debug("p read from polling")
+		p = pollPatterns()
+	else
+		basalt.debug("p loaded from file")
 	end
-	for k, v in pairs(p) do
-		basalt.debug(k, v.name, v.amount)
-	end
+	return p
 end
 
 
 local buttonPolling = mainF:addButton():onClick(basalt.schedule(function() tPatterns = pollPatterns() end)):setSize(10,3):setPosition(1,1):setValue("Poll AE2")
-local buttonSave = mainF:addButton():onClick(function() saveMe(tPatterns) end):setSize(10,3):setPosition(12,1):setValue("Save")
-local buttonAppend = mainF:addButton():onClick(function() appendMe(tPatterns) end):setSize(10,3):setPosition(36,1):setValue("Append")
-local buttonLoad = mainF:addButton():onClick(loadMe):setSize(10,3):setPosition(23,1):setValue("Load")
 fancyButton(buttonPolling)
-fancyButton(buttonSave)
-fancyButton(buttonLoad)
+
+local buttonAppend = mainF:addButton():onClick(function() appendMe(tPatterns) end):setSize(10,3):setPosition(36,1):setValue("Append")
 fancyButton(buttonAppend)
+
+--local buttonSave = mainF:addButton():onClick(function() saveMe(tPatterns) end):setSize(10,3):setPosition(12,1):setValue("Save")
+--fancyButton(buttonSave)
 
 local function myMain()
 	while true do
 		os.sleep(1)
 	end
 end
-	
--- ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+tPatterns = init() -- try to read meBridge.txt if it exists, if not call polling function
+-- -------------------------------------------------------------------------------------------------------------------
 --basalt.debug("Hi")
 parallel.waitForAll(basalt.autoUpdate, myMain)
